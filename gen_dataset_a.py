ba1a59816483f9bd24ad841a34c7fedab601e3eb
TrojanDaniel <ds.young222@gmail.com>
2020-10-29 23:22:40 -0700

    Update gen_dataset_a.py

--- gen_dataset_a.py
+++ gen_dataset_a.py
@@ -6,6 +6,7 @@ import random
 import json
 
 random_num=[random.randint(0,100) for i in range(1000)]
+print(random_num)
 a_1= json.dumps(random_num)
 file_a= open('dataset_a.json', 'w')
 file_a.write(a_1)
