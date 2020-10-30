f9aaf837c9fbfd26bb744069340bbcf492b45b27
TrojanDaniel <ds.young222@gmail.com>
2020-10-30 03:28:28 -0700

    Update gen_dataset_b.py

--- gen_dataset_b.py
+++ gen_dataset_b.py
@@ -6,6 +6,7 @@ file_1=open('dataset_a.json','r')
 rand_nums=file_1.read()
 rand_nums_list=json.loads(rand_nums)
 b_1=[i*3+6 for i in rand_nums_list]
+print(b_1)
 json_b1=json.dumps(b_1)
 file_b=open('dataset_b.json','w')
 file_b.write(json_b1)
