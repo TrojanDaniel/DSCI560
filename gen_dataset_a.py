306a7904b9fd4268560f1da91abca60b54288b7f
TrojanDaniel <ds.young222@gmail.com>
2020-10-30 05:31:54 -0700

    add visualization

--- gen_dataset_a.py
+++ gen_dataset_a.py
@@ -4,9 +4,12 @@
 
 import random
 import json
-
+import matplotlib.pyplot as plt
 random_num=[random.randint(0,100) for i in range(1000)]
 print(random_num)
+plt.title('visualization_a')
+plt.scatter(range(len(random_num)),random_num,c='c',s=10)
+plt.show()
 a_1= json.dumps(random_num)
 file_a= open('dataset_a.json', 'w')
 file_a.write(a_1)
